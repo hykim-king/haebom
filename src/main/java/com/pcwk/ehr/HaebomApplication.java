f40c091062b4b6608ad037ac718763bb099415fd
minsu7463 <117146856+minsu7463@users.noreply.github.com>
2026-02-04 12:24:26 +0900

    upload_project_0204

---
+++ src/main/java/com/pcwk/ehr/HaebomApplication.java
@@ -0,0 +1,13 @@
+package com.pcwk.ehr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaebomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaebomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pcwk/ehr/HaebomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pcwk.ehr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaebomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

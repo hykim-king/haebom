aa2b51ca17c5fb14d2a3d524722f894e635b9545
donghan <waysd14@gmail.com>
2026-02-05 17:40:45 +0900

    공지시항 VO, Mapper.java 문의게시판 VO, Mapper.java 생성완료

--- src/main/java/com/pcwk/ehr/notice/NoticeVO.java
+++ src/main/java/com/pcwk/ehr/notice/NoticeVO.java
@@ -4,9 +4,9 @@ import lombok.*;
 
 import java.time.LocalDateTime;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+@Data // Getter, Setter, RequiredArgsConstructor, ToString, EqualsAndHashCode를 한 번에 다 만들어줌.
+@AllArgsConstructor // 모든 필드(변수)를 파라미터로 받는 생성자를 자동으로 만듬.
+@NoArgsConstructor // 파라미터가 없는 기본 생성자를 자동으로 만듬.
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
 public class NoticeVO extends DTO{
@@ -16,5 +16,6 @@ public class NoticeVO extends DTO{
 	public String notice_content;			// 공지사항 내용
 	public LocalDateTime notice_reg;		// 공지사항 등록일
 	public LocalDateTime notice_mog;		// 공지사항 수정일
-	public int user_sid;					// 사용자 고유번호
+	// public int user_sid;					// 사용자 고유번호
+
 }
--- src/main/java/com/pcwk/ehr/support/SupportVO.java
+++ src/main/java/com/pcwk/ehr/support/SupportVO.java
@@ -4,9 +4,9 @@ import lombok.*;
 
 import java.time.LocalDateTime;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+@Data // Getter, Setter, RequiredArgsConstructor, ToString, EqualsAndHashCode를 한 번에 다 만들어줌.
+@AllArgsConstructor // 모든 필드(변수)를 파라미터로 받는 생성자를 자동으로 만듬.
+@NoArgsConstructor // 파라미터가 없는 기본 생성자를 자동으로 만듬.
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
 public class SupportVO extends DTO{
@@ -16,6 +16,5 @@ public class SupportVO extends DTO{
 	public LocalDateTime sup_reg;		// 문의사항 등록일
 	public String answer;				// 문의사항 답변 내용
 	public LocalDateTime answer_dt; 	// 문의사항 답변 시간
-	public int user_sid;				// 사용자 고유번호
-
+	// public int user_sid;				// 사용자 고유번호
 }

ff6c9f6f6c1902be50af96529b79ecb11301f63a
junyeop19 <wnsduq0928@naver.com>
2026-02-24 03:14:10 +0900

    feat : 관계 dao테스트 추가

--- src/main/java/com/pcwk/ehr/attachfile/AttachFileMapper.java
+++ src/main/java/com/pcwk/ehr/attachfile/AttachFileMapper.java
@@ -2,7 +2,18 @@ package com.pcwk.ehr.attachfile;
 
 import com.pcwk.ehr.cmn.WorkDiv;
 import com.pcwk.ehr.domain.AttachFileVO;
+import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
+@Mapper
 public interface AttachFileMapper extends WorkDiv<AttachFileVO> {
 
+    List<AttachFileVO> getFileList(AttachFileVO param);
+
+    List<AttachFileVO> getAll();
+
+    int getCount();
+
+    int deleteAll();
 }

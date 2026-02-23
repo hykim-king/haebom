ed4bbb8a3cdc263c033d25b2ee74d02e5af3a520
minsu7463 <117146856+minsu7463@users.noreply.github.com>
2026-02-23 22:55:34 +0900

    user:add_feat_Certified_email

---
+++ src/main/java/com/pcwk/ehr/user/MailService.java
@@ -0,0 +1,28 @@
+package com.pcwk.ehr.user;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class MailService {
+
+    private final JavaMailSender mailSender;
+
+    public String generateCode() {
+        int n = (int) (Math.random() * 900000) + 100000; // 100000~999999
+        return String.valueOf(n);
+    }
+
+    public void sendVerificationCode(String to, String code) {
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo(to);
+        msg.setSubject("[해봄트립] 이메일 인증번호 안내");
+        msg.setText("인증번호는 [" + code + "] 입니다. 3분 이내 입력해주세요.");
+
+        mailSender.send(msg);
+    }
+}

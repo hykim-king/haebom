afee19ee77a3f5cfacab596a364b0e39e70646d7
junyeop <youp0928@gmail.com>
2026-02-26 20:25:54 +0900

    feat : main추가

--- src/main/java/com/pcwk/ehr/trip/TripMapper.java
+++ src/main/java/com/pcwk/ehr/trip/TripMapper.java
@@ -3,9 +3,18 @@ package com.pcwk.ehr.trip;
 import com.pcwk.ehr.cmn.WorkDiv;
 import com.pcwk.ehr.domain.TripVO;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface TripMapper extends WorkDiv<TripVO> {
     int getCount();
+
+        //인기 관광지 top3
+    List<TripVO> selectTop3Popular();
+
+    //지역별 관광지 랜덤 추천
+    List<TripVO> selectRandomPerRegion();
+    
 }
